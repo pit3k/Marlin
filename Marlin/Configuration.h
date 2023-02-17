173beb1edd6e1da17d164c64ea1ddfc3ee9de8be
Piotr Paczynski <ppaczyn@gmail.com>
2023-02-17 17:57:27 +0100

    new bed size after changing rod-blocks

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1758,8 +1758,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 205
-#define Y_BED_SIZE 205
+#define X_BED_SIZE 210
+#define Y_BED_SIZE 210
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1767,7 +1767,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 205
+#define Z_MAX_POS 195
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0

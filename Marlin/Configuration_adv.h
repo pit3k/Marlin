5c94481c6a7cacda97ec59564d1f998ce70b6410
Piotr Paczynski <ppaczyn@gmail.com>
2023-04-02 20:58:57 +0200

    set PWM range for new sunon fans

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -608,8 +608,8 @@
  *
  * Define one or both of these to override the default 0-255 range.
  */
-//#define FAN_MIN_PWM 50
-//#define FAN_MAX_PWM 128
+#define FAN_MIN_PWM 20
+#define FAN_MAX_PWM 128
 
 /**
  * Fan Fast PWM

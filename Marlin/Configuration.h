4f1dc0fd667715f77480c82b8ece0b2719cd86a3
Piotr Paczynski <ppaczyn@gmail.com>
2026-02-01 22:37:43 +0100

    z steps 32->16, e steps 16->8

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1196,7 +1196,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 32*200/40, 32*200/40, 32*200.0/3, 16*15.875 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 32*200/40, 32*200/40, 16*200.0/3, 8*15.875 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2776,7 +2776,7 @@
   #if AXIS_IS_TMC_CONFIG(Z)
     #define Z_CURRENT       850
     #define Z_CURRENT_HOME  Z_CURRENT
-    #define Z_MICROSTEPS     32
+    #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
     #define Z_CHAIN_POS      -1
     //#define Z_INTERPOLATE  true
@@ -2875,7 +2875,7 @@
 
   #if AXIS_IS_TMC_CONFIG(E0)
     #define E0_CURRENT      1000
-    #define E0_MICROSTEPS    16
+    #define E0_MICROSTEPS    8
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
     //#define E0_INTERPOLATE true
